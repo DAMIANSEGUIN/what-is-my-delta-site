d760c73660e24bd1fcab9c752ef075978d49de5f
DAMIANSEGUIN <38871298+DAMIANSEGUIN@users.noreply.github.com>
2025-08-21 16:17:25 -0400

    Add Tawk widget via next/script (auto)

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import Script from "next/script";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script id="tawk" strategy="afterInteractive" src="https://embed.tawk.to/68a767d97ebce11927982120/1j36uravb" />
+    </>
+  );
+}
